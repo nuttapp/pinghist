0a098cacdb8492bbc638b40beb52d59c4aa1b0d7
Greg Ingelmo <git@ingelmo.org>
2015-01-21 09:52:41 -0500

    Compress output of GoConvey, remove some colors, add some back

--- test.sh
+++ test.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
-# go test ./... -run Unit -v
-go test ./... -run Integration -v
+unset REPORTTIME 
+
+go test -v -run Unit ./... | sed "s/.* assertion.*/[0m/" | grep -v -E "^($|\?|PASS|ok)" | sed 's/^\[.*//g' | egrep -v '^[[:space:]]*$' |sed 's/===/\
+/g'  | sed "s/---//g" | sed "s/Failures:/[38;5;160mFAIL/g" | sed "s/RUN/[0mRUN/g"
+
+go test -v -run Inte ./... | sed "s/.* assertion.*/[0m/" | grep -v -E "^($|\?|PASS|ok)" | sed 's/^\[.*//g' | egrep -v '^[[:space:]]*$' |sed 's/===/\
+/g'  | sed "s/---//g" | sed "s/Failures:/[38;5;160mFAIL/g" | sed "s/RUN/[0mRUN/g"
+
+
