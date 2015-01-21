d410010b492f2d34833bd1b1090e0d74bd12f444
Greg Ingelmo <git@ingelmo.org>
2015-01-21 10:01:17 -0500

    Add comments to test script

--- test.sh
+++ test.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
+# Run all tests...
+
+# This oneliner monstrosity removes extra lines and vertical junk from the output of GoConvey. 
+# It selectively removes colorized outout, and adds it back when a test fails (red)
 unset REPORTTIME 
 
+# Run unit tests
 go test -v -run Unit ./... | sed "s/.* assertion.*/[0m/" | grep -v -E "^($|\?|PASS|ok)" | sed 's/^\[.*//g' | egrep -v '^[[:space:]]*$' |sed 's/===/\
 /g'  | sed "s/---//g" | sed "s/Failures:/[38;5;160mFAIL/g" | sed "s/RUN/[0mRUN/g"
 
+# Run Integration tests
 go test -v -run Inte ./... | sed "s/.* assertion.*/[0m/" | grep -v -E "^($|\?|PASS|ok)" | sed 's/^\[.*//g' | egrep -v '^[[:space:]]*$' |sed 's/===/\
 /g'  | sed "s/---//g" | sed "s/Failures:/[38;5;160mFAIL/g" | sed "s/RUN/[0mRUN/g"
 
