26d5c4d7ae9cf6c9525fffbf6d1d381ec61008ad
Greg Ingelmo <git@ingelmo.org>
2015-01-24 13:02:52 -0500

    Update location of test db

--- db_info.sh
+++ db_info.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-size=`du -h pinghist.db`
-kcount=`bolt keys pinghist.db pings_by_minute | wc -l | tr -d ' '`
+size=`du -h ./dal/pinghist.db`
+kcount=`bolt keys ./dal/pinghist.db pings_by_minute | wc -l | tr -d ' '`
 echo "$size: $kcount keys"
