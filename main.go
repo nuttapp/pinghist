f88deaff991c6de2a52ee705b981ae5c0f14befc
Greg Ingelmo <git@ingelmo.org>
2015-01-29 22:26:11 -0500

    Add first stab at parsing cmd line params

--- main.go
+++ main.go
@@ -1,19 +1,88 @@
 package main
 
 import (
+	"flag"
 	"fmt"
-
-	"github.com/nuttapp/pinghist/ping"
+	"log"
+	"time"
 )
 
+var (
+	start            string
+	end              string
+	inputTimeFormats = []string{
+		"01/02 03:04 pm",
+		"1/02 03:04 pm",
+		"01/02 03:04pm",
+		"1/02 03:04pm",
+		"01/02 3:04 pm",
+		"1/02 3:04 pm",
+		"01/02 3:04pm",
+		"1/02 3:04pm",
+		"01/02 3 pm",
+		"1/02 3pm",
+		"03:04 pm",
+		"3:04 pm",
+		"03:04pm",
+		"3:04pm",
+		"01/02 15:04",
+		"1/02 15:04",
+		"15:04",
+	}
+)
+
+const (
+	timeFormat      = "01/02 03:04 pm"
+	timeShortformat = "03:04 pm"
+)
+
+func init() {
+	t := time.Now().Add(-1 * time.Hour)
+	td := time.Date(t.Year(), t.Month(), t.Day(), t.Hour(), 0, 0, 0, t.Location())
+	defaultSart := td.Format(timeFormat)
+	defaultEnd := td.Add(1 * time.Hour).Format(timeFormat)
+	const (
+		startUsage = "The time to start querying ping times"
+		endUsage   = "The time to end querying ping times (inclusive)"
+	)
+
+	flag.StringVar(&start, "start", defaultSart, startUsage)
+	flag.StringVar(&end, "s", defaultEnd, startUsage+" (shorthand)")
+
+	flag.StringVar(&start, "end", defaultSart, endUsage)
+	flag.StringVar(&end, "e", defaultEnd, endUsage+" (shorthand)")
+}
+
 func main() {
-	pr, err := ping.Ping("127.0.0.1")
-	if err != nil {
-		fmt.Println(err)
-		return
+	// fmt.Printf("start: %s\nend:   %s\n", start, end)
+	flag.Parse()
+
+	l := time.Now().Location()
+	var t time.Time
+	var err error
+	for _, f := range inputTimeFormats {
+		t, err = time.ParseInLocation(f, start, l)
+		if err == nil {
+			break
+		}
 	}
 
-	fmt.Printf("%#v", pr)
+	if err != nil {
+		log.Fatal("unkown tie format")
+	}
+
+	fmt.Printf("start: %s\nend:   %s\n", t.Format(timeFormat), end)
+	fmt.Println(t)
+	// pr, err := ping.Ping("127.0.0.1")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// 	return
+	// }
+	//
+	// fmt.Printf("%#v", pr)
 
 	fmt.Println("END")
+}
+
+func parseFormat(dateTime string) {
 }
