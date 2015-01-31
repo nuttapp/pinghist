5ed736997051556afba89b1569ac6287aab4e0a2
Flew H <flewhigh@nuttapp.com>
2015-01-31 13:14:48 -0500

    Fix dal integration tests

--- dal/dal_test.go
+++ dal/dal_test.go
@@ -1,7 +1,6 @@
 package dal
 
 import (
-	"errors"
 	"fmt"
 	"log"
 	"math/rand"
@@ -17,139 +16,126 @@ import (
 func Test_dal_integration(t *testing.T) {
 
 	Convey("dal", t, func() {
+		resetTestDB() // run before every Convey(...)
+		Reset(func() {
+			os.Remove("pinghist.db")
+		})
 
-		// SkipConvey("SavePing()", func() {
-		// 	resetTestDB()
-		// 	Reset(func() {
-		// 		os.Remove("pinghist.db")
-		// 	})
-		//
-		// 	ip := "127.0.0.1"
-		// 	l, _ := time.LoadLocation("UTC")
-		// 	startTime := time.Date(2015, time.January, 1, 12, 30, 0, 0, l) // 2015-01-01 12:30:00 +0000 UTC
-		// 	responseTime := float32(1.1)
-		//
-		// 	Convey("Should create one key w/ 1 ping", func() {
-		// 		err := SavePing(ip, startTime, responseTime)
-		// 		So(err, ShouldBeNil)
-		//
-		// 		keys := getPingKeys()
-		// 		So(keys[0], ShouldEqual, string(GetPingKey(ip, startTime)))
-		// 	})
-		// 	Convey("Should create one key when 2 pings are < 1 minute apart", func() {
-		// 		startTime2 := startTime.Add(1 * time.Second) // add a second
-		//
-		// 		err := SavePing(ip, startTime, responseTime)
-		// 		So(err, ShouldBeNil)
-		//
-		// 		err = SavePing(ip, startTime2, responseTime)
-		// 		So(err, ShouldBeNil)
-		//
-		// 		keys := getPingKeys()
-		// 		So(keys[0], ShouldEqual, string(GetPingKey(ip, startTime)))
-		// 	})
-		// 	Convey("Should create 2 keys when 2 pings are > 1 minute apart", func() {
-		// 		startTime2 := startTime.Add(1 * time.Minute) // add a minute
-		//
-		// 		err := SavePing(ip, startTime, responseTime)
-		// 		So(err, ShouldBeNil)
-		//
-		// 		err = SavePing(ip, startTime2, responseTime)
-		// 		So(err, ShouldBeNil)
-		//
-		// 		keys := getPingKeys()
-		// 		So(keys[0], ShouldEqual, string(GetPingKey(ip, startTime)))
-		// 		So(keys[1], ShouldEqual, string(GetPingKey(ip, startTime2)))
-		// 	})
-		// })
-		//
-		// SkipConvey("GetPings()", func() {
-		// 	resetTestDB()
-		// 	seedTestDB()
-		// 	Reset(func() {
-		// 		os.Remove("pinghist.db")
-		// 	})
-		//
-		// 	end := time.Now()
-		// 	start := end.Add(-25 * time.Hour)
-		// 	groups, err := GetPings("127.0.0.1", start, end, 1*time.Hour)
-		//
-		// 	So(err, ShouldBeNil)
-		// 	So(len(groups), ShouldEqual, 24) // there should be 1 group per hour
-		//
-		// 	totalPings := 0
-		// 	for _, group := range groups {
-		// 		totalPings += group.Count
-		// 	}
-		//
-		// 	So(totalPings, ShouldEqual, 86400) // there should 1 ping for every second in a day
-		//
-		// 	// fmt.Println()
-		// 	// for i, g := range groups {
-		// 	// 	avg := g.TotalTime / float32(g.Count)
-		// 	// 	fmt.Printf("%d: %s, count: %d, avg: %.2f, min: %.2f, max %.2f\n",
-		// 	// 		i+1, g.Timestamp.Format(time.RFC3339), g.Count, avg, g.MinTime, g.MaxTime)
-		// 	// 	// for _, key := range g.Keys {
-		// 	// 	// 	fmt.Printf("key: %s\n", key)
-		// 	// 	// }
-		// 	// }
-		// })
+		Convey("SavePing()", func() {
+			ip := "127.0.0.1"
+			l, _ := time.LoadLocation("UTC")
+			startTime := time.Date(2015, time.January, 1, 12, 30, 0, 0, l) // 2015-01-01 12:30:00 +0000 UTC
+			responseTime := float32(1.1)
 
-		Convey("Tablewriter()", func() {
-			createTestDB()
-			// resetTestDB()
-			seedTestDB()
-			// Reset(func() {
-			// 	os.Remove("pinghist.db")
-			// })
+			Convey("should create one key w/ 1 ping", func() {
+				err := SavePing(ip, startTime, responseTime)
+				So(err, ShouldBeNil)
 
-			fmt.Println("\n")
-
-			table := tablewriter.NewWriter(os.Stdout)
-			table.SetHeader([]string{
-				"Time",
-				"min",
-				"avg",
-				"max",
-				"std dev",
-				"Received",
-				"Lost",
+				keys := getAllPingKeys()
+				So(keys[0], ShouldEqual, string(GetPingKey(ip, startTime)))
 			})
-			// table.SetFooter([]string{"", "", "", "Total", "$146.93"}) // Add Footer
-			table.SetBorder(false) // Set Border to false
-			table.SetAlignment(tablewriter.ALIGN_RIGHT)
+			Convey("should create one key when 2 pings are < 1 minute apart", func() {
+				startTime2 := startTime.Add(1 * time.Second) // add a second
 
-			t := time.Now()
-			fmt.Printf("time.now: %s\n", t.Format(time.Kitchen))
-			end := time.Date(t.Year(), t.Month(), t.Day(), t.Hour(), 0, 0, 0, t.Location())
-			start := end.Add(-25 * time.Hour)
-			groups, _ := GetPings("127.0.0.1", start, end, 1*time.Hour)
-			// location, _ := time.LoadLocation("America/New_York")
+				err := SavePing(ip, startTime, responseTime)
+				So(err, ShouldBeNil)
 
-			totalPings := 0
-			for _, g := range groups {
-				row := []string{
-					// g.Timestamp.In(location).Format(time.Kitchen),
-					g.Timestamp.Format(time.Kitchen),
-					fmt.Sprintf("%.0f ms", g.MinTime),
-					fmt.Sprintf("%.0f ms", g.AvgTime),
-					fmt.Sprintf("%.0f ms", g.MaxTime),
-					fmt.Sprintf("%.0f ms", g.StdDev),
-					fmt.Sprintf("%d", g.Received),
-					"0",
-				}
-				table.Append(row)
-				totalPings += g.Received
-			}
-			// table.AppendBulk(data)                                // Add Bulk Data
-			table.Render()
+				err = SavePing(ip, startTime2, responseTime)
+				So(err, ShouldBeNil)
+
+				keys := getAllPingKeys()
+				So(keys[0], ShouldEqual, string(GetPingKey(ip, startTime)))
+			})
+			Convey("should create 2 keys when 2 pings are > 1 minute apart", func() {
+				startTime2 := startTime.Add(1 * time.Minute) // add a minute
+
+				err := SavePing(ip, startTime, responseTime)
+				So(err, ShouldBeNil)
+
+				err = SavePing(ip, startTime2, responseTime)
+				So(err, ShouldBeNil)
+
+				keys := getAllPingKeys()
+				So(keys[0], ShouldEqual, string(GetPingKey(ip, startTime)))
+				So(keys[1], ShouldEqual, string(GetPingKey(ip, startTime2)))
+			})
+		})
+
+		Convey("GetPings()", func() {
+			l := time.Now().Location()
+			tfmt := "01/02/06 03:04:05 pm"
+
+			Convey("should return 24 groups, 1 hour in each group", func() {
+				seedTestDB("01/03/15 04:00:00 pm", "01/04/15 06:00:00 pm")
+
+				start, _ := time.ParseInLocation(tfmt, "01/03/15 05:00:00 pm", l)
+				endti, _ := time.ParseInLocation(tfmt, "01/04/15 05:00:00 pm", l)
+				groupBy := 1 * time.Hour
+				// fmt.Printf("%s - %s\n", start.Format(tfmt), endti.Format(tfmt))
+
+				groups, err := GetPings("127.0.0.1", start, endti, groupBy)
+				So(err, ShouldBeNil)
+				So(len(groups), ShouldEqual, 24)            // there should be 1 group per hour
+				So(sumReceived(groups), ShouldEqual, 86400) // there should 1 ping for every second in a day
+				So(groups[0].Start, ShouldHappenOnOrAfter, start)
+				So(groups[len(groups)-1].End, ShouldHappenOnOrBefore, endti)
+				// writeTable(groups)
+			})
+			Convey("should return  4 groups, 15 minutes in each group", func() {
+				seedTestDB("01/03/15 03:00:00 pm", "01/03/15 06:00:00 pm")
+
+				start, _ := time.ParseInLocation(tfmt, "01/03/15 04:00:00 pm", l)
+				endti, _ := time.ParseInLocation(tfmt, "01/03/15 05:00:00 pm", l)
+				groupBy := 15 * time.Minute
+
+				groups, err := GetPings("127.0.0.1", start, endti, groupBy)
+				So(err, ShouldBeNil)
+				So(len(groups), ShouldEqual, 4)
+				So(groups[0].Start, ShouldHappenOnOrAfter, start)
+				So(groups[len(groups)-1].End, ShouldHappenOnOrBefore, endti)
+				So(sumReceived(groups), ShouldEqual, 3600) // there should 1 ping for every second in a day
+			})
+			Convey("should return 60 groups, 1 second in each group", func() {
+				seedTestDB("01/03/15 03:00:00 pm", "01/03/15 03:05:00 pm")
+
+				start, _ := time.ParseInLocation(tfmt, "01/03/15 03:00:00 pm", l)
+				endti, _ := time.ParseInLocation(tfmt, "01/03/15 03:01:00 pm", l)
+				groupBy := 1 * time.Second
+
+				groups, err := GetPings("127.0.0.1", start, endti, groupBy)
+				So(err, ShouldBeNil)
+				So(sumReceived(groups), ShouldEqual, 60)
+				So(groups[0].Start, ShouldHappenOnOrAfter, start)
+				So(groups[len(groups)-1].End, ShouldHappenOnOrBefore, endti)
+			})
+			Convey("should return 30 groups, 1 second in each group", func() {
+				seedTestDB("01/03/15 02:00:00 pm", "01/03/15 04:00:00 pm")
+
+				start, _ := time.ParseInLocation(tfmt, "01/03/15 03:00:00 pm", l)
+				endti, _ := time.ParseInLocation(tfmt, "01/03/15 03:00:30 pm", l)
+				groupBy := 1 * time.Second
+
+				groups, err := GetPings("127.0.0.1", start, endti, groupBy)
+				So(err, ShouldBeNil)
+				So(len(groups), ShouldEqual, 30)
+				So(sumReceived(groups), ShouldEqual, 30)
+				So(groups[0].Start, ShouldHappenOnOrAfter, start)
+				So(groups[len(groups)-1].End, ShouldHappenOnOrBefore, endti)
+			})
 		})
 	})
 }
 
-// seedTestDB will seed the db with 24 hours of pings for every second
-// it adds 1441 rows to the pings_by_minute bucket
-func seedTestDB() {
+func sumReceived(groups []*PingGroup) int {
+	totalPings := 0
+	for _, group := range groups {
+		totalPings += group.Received
+	}
+	return totalPings
+}
+
+// seedTestDB will seed the db every second betwene the given times
+func seedTestDB(startTime, endTime string) {
 	db, err := bolt.Open("pinghist.db", 0600, nil)
 	defer db.Close()
 	if err != nil {
@@ -157,41 +143,34 @@ func seedTestDB() {
 	}
 
 	ip := "127.0.0.1"
-	maxRes := float32(1500.0)
-	minRes := float32(5.0)
-	now := time.Now()
-	startTime := now.Add(-24 * time.Hour)
+	maxRes, minRes := float32(1500.0), float32(5.0)
+	rand.Seed(time.Now().UnixNano())
+
+	l := time.Now().Location()
+	tfmt := "01/02/06 03:04:05 pm"
+	start, _ := time.ParseInLocation(tfmt, startTime, l)
+	end, _ := time.ParseInLocation(tfmt, endTime, l)
 
 	err = db.Update(func(tx *bolt.Tx) error {
-		pings := tx.Bucket([]byte("pings_by_minute"))
-		if pings == nil {
-			return errors.New("Couldn't find pings_by_minute bucket")
-		}
-
-		// var finalT time.Time
 		// pt == ping timestamp
-		for pt := startTime; pt.Sub(now) != 0; pt = pt.Add(1 * time.Second) {
+		for pt := start; pt.Sub(end) != 0; pt = pt.Add(1 * time.Second) {
 			resTime := rand.Float32()*(maxRes-minRes) + minRes
 
 			err := SavePingWithTransaction(ip, pt, resTime, tx)
 			if err != nil {
 				return err
 			}
-
-			// finalT = pt
 		}
 
-		// fmt.Printf("final time key: %s\n", finalT)
 		return nil
 	})
 
 	if err != nil {
-		fmt.Println(err)
 		log.Fatal(err)
 	}
 }
 
-func getPingKeys() []string {
+func getAllPingKeys() []string {
 	db, err := bolt.Open("pinghist.db", 0600, nil)
 	defer db.Close()
 	if err != nil {
@@ -213,11 +192,40 @@ func getPingKeys() []string {
 	return keys
 }
 
-func DeleteDB() {
-	// os.Remove("pinghist.db")
+func writeTable(groups []*PingGroup) {
+	fmt.Println("\n")
+
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{
+		"Time",
+		"min",
+		"avg",
+		"max",
+		"std dev",
+		"Received",
+		"Lost",
+	})
+
+	table.SetBorder(false) // Set Border to false
+	table.SetAlignment(tablewriter.ALIGN_RIGHT)
+
+	for _, g := range groups {
+		row := []string{
+			fmt.Sprintf("%s", g.Start.Format("01/02 03:04pm")),
+			fmt.Sprintf("%.0f ms", g.MinTime),
+			fmt.Sprintf("%.0f ms", g.AvgTime),
+			fmt.Sprintf("%.0f ms", g.MaxTime),
+			fmt.Sprintf("%.0f ms", g.StdDev),
+			fmt.Sprintf("%d", g.Received),
+			"0",
+		}
+		table.Append(row)
+	}
+	// table.AppendBulk(data)                                // Add Bulk Data
+	table.Render()
 }
 
-var boltBuckets = []string{"hosts", "pings_by_minute"}
+var boltBuckets = []string{"pings_by_minute"}
 
 // createTestDB will create an empty Bolt DB and buckets
 func createTestDB() {
