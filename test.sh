ffae6fc5f95ba3a4c5b2d7415f978dfaece19fdc
Greg Ingelmo <git@ingelmo.org>
2015-01-31 13:33:52 -0500

    Add test script comments

--- test.sh
+++ test.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 # Run all tests...
-
-# This oneliner monstrosity removes extra lines and vertical junk from the output of GoConvey. 
-# It selectively removes colorized outout, and adds it back when a test fails (red)
+# The sed/grep monstrosity removes extra lines and vertical junk from the verbose output of GoConvey. 
+# It selectively removes colorized outout, but adds it back when a test fails.
 unset REPORTTIME 
 
-# Run unit tests
+# go test -v ./... -run dal_integ
+
+# # Run unit tests
 go test -v -run unit ./... | sed "s/.* assertion.*/[0m/" | grep -v -E "^($|\?|PASS|ok)" | sed 's/^\[.*//g' | egrep -v '^[[:space:]]*$' |sed 's/===/\
 /g'  | sed "s/---//g" | sed "s/Failures:/[38;5;160mFAIL/g" | sed "s/RUN/[0mRUN/g"
-
-# Run Integration tests
+#
+# # Run Integration tests
 go test -v -run inte ./... | sed "s/.* assertion.*/[0m/" | grep -v -E "^($|\?|PASS|ok)" | sed 's/^\[.*//g' | egrep -v '^[[:space:]]*$' |sed 's/===/\
 /g'  | sed "s/---//g" | sed "s/Failures:/[38;5;160mFAIL/g" | sed "s/RUN/[0mRUN/g"
 
