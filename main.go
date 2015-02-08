7a38e8c6bc5d34ab2cbc76bb358e144a05c23b33
Greg Ingelmo <git@ingelmo.org>
2015-02-07 21:54:15 -0500

    Add func to parse dates out of cmd line and tests

--- main.go
+++ main.go
@@ -33,6 +33,7 @@ var (
 		"01/02 15:04",
 		"01/2 15:04",
 		"1/02 15:04",
+		"1/2 15:04",
 		"15:04",
 
 		time.RFC3339,
@@ -51,7 +52,7 @@ func init() {
 	defaultEnd := td.Add(1 * time.Hour).Format(timeFormat)
 	const (
 		startUsage = "The time to start querying ping times"
-		endUsage   = "The time to end querying ping times (inclusive)"
+		endUsage   = "The time to end querying ping times (all time up to this point)"
 	)
 
 	flag.StringVar(&start, "start", defaultSart, startUsage)
@@ -92,5 +93,29 @@ func main() {
 	fmt.Println("END")
 }
 
-func parseFormat(dateTime string) {
+func ParseTime(str string) (time.Time, error) {
+	now := time.Now()
+	t := time.Time{}
+	var err error
+
+	isShortTime := false
+	for _, tfmt := range inputTimeFormats {
+		t, err = time.ParseInLocation(tfmt, str, time.Local)
+		if err == nil {
+			if tfmt == "03:04 pm" || tfmt == "3:04 pm" || tfmt == "15:04" {
+				isShortTime = true
+			}
+			break
+		}
+		// fmt.Printf("%s \n", tfmt)
+	}
+
+	// if no year or month or day was given fill those in with todays date
+	if isShortTime {
+		t = time.Date(now.Year(), now.Month(), now.Day(), t.Hour(), t.Minute(), t.Second(), 0, time.Local)
+	} else if t.Year() == 0 { // no year was specified
+		t = time.Date(now.Year(), t.Month(), t.Day(), t.Hour(), t.Minute(), t.Second(), 0, time.Local)
+	}
+
+	return t, err
 }
---
+++ main_test.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+	"time"
+
+	. "github.com/smartystreets/goconvey/convey"
+)
+
+func Test_main_unit(t *testing.T) {
+	Convey("main", t, func() {
+		parts := strings.Split(time.Now().Format("2006-01-02-07:00"), "-")
+		y, m, d, z := parts[0], parts[1], parts[2], parts[3]
+
+		testTable := map[string]string{
+			"01/01 01:52 pm": fmt.Sprintf("%s-01-01T13:52:00-%s", y, z),       // full
+			"1/01 01:00 pm":  fmt.Sprintf("%s-01-01T13:00:00-%s", y, z),       // short day/month
+			"01/1 01:00 pm":  fmt.Sprintf("%s-01-01T13:00:00-%s", y, z),       // short day/month
+			"1/1 01:00 pm":   fmt.Sprintf("%s-01-01T13:00:00-%s", y, z),       // short day/month
+			"01/01 1:00 pm":  fmt.Sprintf("%s-01-01T13:00:00-%s", y, z),       // short hour
+			"1/01 1:00 pm":   fmt.Sprintf("%s-01-01T13:00:00-%s", y, z),       // short hour
+			"01/1 1:00 pm":   fmt.Sprintf("%s-01-01T13:00:00-%s", y, z),       // short hour
+			"1/1 1:00 pm":    fmt.Sprintf("%s-01-01T13:00:00-%s", y, z),       // short hour
+			"01:52 pm":       fmt.Sprintf("%s-%s-%sT13:52:00-%s", y, m, d, z), // time
+			"1:52 pm":        fmt.Sprintf("%s-%s-%sT13:52:00-%s", y, m, d, z), // time
+			"01/01 13:52":    fmt.Sprintf("%s-01-01T13:52:00-%s", y, z),       // 24hr time
+			"1/01 13:52":     fmt.Sprintf("%s-01-01T13:52:00-%s", y, z),       // 24hr time
+			"01/1 13:52":     fmt.Sprintf("%s-01-01T13:52:00-%s", y, z),       // 24hr time
+			"1/1 13:52":      fmt.Sprintf("%s-01-01T13:52:00-%s", y, z),       // 24hr time
+			"13:52":          fmt.Sprintf("%s-%s-%sT13:52:00-%s", y, m, d, z), // 24hr time
+		}
+
+		for teststr, goodstr := range testTable {
+			Convey("Given "+teststr, func() {
+				Convey("Time should equal "+goodstr, func() {
+					t, err := ParseTime(teststr)
+					So(err, ShouldBeNil)
+					So(t.Format(time.RFC3339), ShouldEqual, goodstr)
+				})
+			})
+		}
+	})
+}
