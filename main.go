beecb0caef85ba74ced44f298f5ec76fab6f94b7
Greg Ingelmo <git@ingelmo.org>
2015-02-08 19:56:01 -0500

    First stab at handling command line arguments

--- main.go
+++ main.go
@@ -4,12 +4,21 @@ import (
 	"flag"
 	"fmt"
 	"log"
+	"os"
+	"os/signal"
 	"time"
+
+	"github.com/nuttapp/pinghist/dal"
+	"github.com/nuttapp/pinghist/ping"
+	"github.com/olekukonko/tablewriter"
 )
 
 var (
+	host             string
+	showExamples     bool
 	start            string
 	end              string
+	groupBy          string
 	inputTimeFormats = []string{
 		// full
 		"01/02 03:04 pm",
@@ -41,56 +50,103 @@ var (
 )
 
 const (
-	timeFormat      = "01/02 03:04 pm"
+	timeFormat      = "01/02/2006 03:04 pm"
 	timeShortformat = "03:04 pm"
 )
 
 func init() {
-	t := time.Now().Add(-1 * time.Hour)
-	td := time.Date(t.Year(), t.Month(), t.Day(), t.Hour(), 0, 0, 0, t.Location())
-	defaultSart := td.Format(timeFormat)
-	defaultEnd := td.Add(1 * time.Hour).Format(timeFormat)
 	const (
-		startUsage = "The time to start querying ping times"
-		endUsage   = "The time to end querying ping times (all time up to this point)"
+		hostUsage         = "The host IP or hostname to ping"
+		showExamplesUsage = "Show example usage"
+		startUsage        = "The time to start querying ping times"
+		endUsage          = "The time to end querying ping times (all time up to this point)"
+		groupUsage        = "The duration by which to group the results, supports (s)econds, (m)inutes, (h)ours"
 	)
 
-	flag.StringVar(&start, "start", defaultSart, startUsage)
-	flag.StringVar(&end, "s", defaultEnd, startUsage+" (shorthand)")
+	flag.BoolVar(&showExamples, "examples", false, showExamplesUsage)
 
-	flag.StringVar(&start, "end", defaultSart, endUsage)
-	flag.StringVar(&end, "e", defaultEnd, endUsage+" (shorthand)")
+	flag.StringVar(&host, "host", "", hostUsage)
+	flag.StringVar(&host, "h", "", "-host")
+
+	flag.StringVar(&start, "start", "", startUsage)
+	flag.StringVar(&start, "s", "", "-start")
+
+	flag.StringVar(&start, "end", "", endUsage)
+	flag.StringVar(&end, "e", "", "-end")
+
+	flag.StringVar(&groupBy, "groupby", "1h", groupUsage)
+	flag.StringVar(&groupBy, "g", "", "-groupby")
 }
 
 func main() {
-	// fmt.Printf("start: %s\nend:   %s\n", start, end)
 	flag.Parse()
 
-	l := time.Now().Location()
-	var t time.Time
-	var err error
-	for _, f := range inputTimeFormats {
-		t, err = time.ParseInLocation(f, start, l)
-		if err == nil {
-			break
+	if host != "" {
+		PingHost(host)
+		return
+	}
+
+	if showExamples {
+		fmt.Println("examples...")
+		return
+	}
+
+	st, err := ParseTime(start)
+	if err != nil {
+		log.Fatal("Can't parse start time")
+	}
+	et, err := ParseTime(end)
+	if err != nil {
+		log.Fatal("Can't parse end time")
+	}
+
+	d := dal.NewDAL()
+	d.CreateBuckets()
+
+	if groupBy == "" {
+		groupBy = "1m"
+	}
+	dur, err := time.ParseDuration(groupBy)
+	if err != nil {
+		log.Fatal("Can't parse groupby: " + err.Error())
+	}
+	fmt.Printf("st:  %s\n", st)
+	fmt.Printf("ed:  %s\n", et)
+	fmt.Printf("dur: %s\n", dur)
+
+	ip := "127.0.0.1"
+	groups, err := d.GetPings(ip, st, et, dur)
+	if err != nil {
+		log.Fatal("Couldn't retreive pings: %s", err)
+	}
+
+	WriteTable(groups)
+}
+
+func PingHost(host string) {
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+	d := dal.NewDAL()
+
+	for {
+		tick := time.NewTicker(1 * time.Second)
+		select {
+		case <-tick.C:
+			startTime := time.Now()
+			pr, err := ping.Ping(host)
+			if err != nil {
+				fmt.Println(err)
+			} else {
+				fmt.Printf("%s %.2f \n", pr.IP, pr.Time)
+				err := d.SavePing(pr.IP, startTime, float32(pr.Time))
+				if err != nil {
+					log.Fatal(err)
+				}
+			}
+		case <-signalChan:
+			os.Exit(0)
 		}
 	}
-
-	if err != nil {
-		log.Fatal("unkown tie format")
-	}
-
-	fmt.Printf("start: %s\nend:   %s\n", t.Format(timeFormat), end)
-	fmt.Println(t)
-	// pr, err := ping.Ping("127.0.0.1")
-	// if err != nil {
-	// 	fmt.Println(err)
-	// 	return
-	// }
-	//
-	// fmt.Printf("%#v", pr)
-
-	fmt.Println("END")
 }
 
 func ParseTime(str string) (time.Time, error) {
@@ -118,4 +174,34 @@ func ParseTime(str string) (time.Time, error) {
 	}
 
 	return t, err
+}
+
+func WriteTable(groups []*dal.PingGroup) {
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{
+		"Time",
+		"min",
+		"avg",
+		"max",
+		"std dev",
+		"Received",
+		"Lost",
+	})
+
+	table.SetBorder(false) // Set Border to false
+	table.SetAlignment(tablewriter.ALIGN_RIGHT)
+
+	for _, g := range groups {
+		row := []string{
+			fmt.Sprintf("%s", g.Start.In(time.Local).Format("01/02 03:04pm")),
+			fmt.Sprintf("%.0f ms", g.MinTime),
+			fmt.Sprintf("%.0f ms", g.AvgTime),
+			fmt.Sprintf("%.0f ms", g.MaxTime),
+			fmt.Sprintf("%.0f ms", g.StdDev),
+			fmt.Sprintf("%d", g.Received),
+			"0",
+		}
+		table.Append(row)
+	}
+	table.Render()
 }
