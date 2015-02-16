9bbb274a46cdabbbfb941bce1a0db72a25bdeda6
Flew H <flewhigh@nuttapp.com>
2015-02-15 22:56:06 -0500

    Add IPStats type & bucket
    IPStats will be used to summarize information about
    an IP that's hard or time consuming to calculate.

--- dal/ip_stats_test.go
+++ dal/ip_stats_test.go
@@ -7,25 +7,73 @@ import (
 )
 
 func Test_ip_stats_integration(t *testing.T) {
-	dal := NewDAL()
-	dal.CreateBuckets()
+	Convey("IPStats", t, func() {
+		dal := NewDAL()
+		dal.CreateBuckets()
 
-	SkipConvey("IPStats", t, func() {
+		stats := IPStats{
+			IP:           "127.0.0.1",
+			FirstPingKey: "foo",
+			LastPingKey:  "bar",
+			Received:     1,
+			Lost:         2,
+		}
 
-		Convey("Should save IPStats to DB", func() {
-			stats := IPStats{
-				IP:           "127.0.0.1",
-				FirstPingKey: "foo",
-			}
-			err := dal.SaveIPStats(stats)
-			So(err, ShouldBeNil)
+		Convey("SaveIPStats()", func() {
+			Convey("should save IPStats", func() {
+				err := dal.SaveIPStats(stats)
+				So(err, ShouldBeNil)
+			})
+			Convey("should return error with invalid IP", func() {
+				stats.IP = ""
+				err := dal.SaveIPStats(stats)
+				So(err, ShouldNotBeNil)
+				So(err.Error(), ShouldContainSubstring, IPRequiredError)
+			})
+			Convey("should return error when it can't open db ", func() {
+				dal.fileName = ""
+				err := dal.SaveIPStats(stats)
+				So(err, ShouldNotBeNil)
+			})
 		})
 
-		Convey("Should get IPStats from DB", func() {
-			ipStats, err := dal.GetIPStats("127.0.0.1")
-			So(err, ShouldBeNil)
-			So(ipStats, ShouldNotBeNil)
-		})
+		Convey("GetIPStats()", func() {
+			Convey("Should get & update IPStats", func() {
+				err := dal.SaveIPStats(stats)
+				So(err, ShouldBeNil)
+				sSaved, err := dal.GetIPStats(stats.IP)
 
+				So(err, ShouldBeNil)
+				So(sSaved, ShouldNotBeNil)
+				So(sSaved.IP, ShouldEqual, stats.IP)
+				So(sSaved.FirstPingKey, ShouldEqual, stats.FirstPingKey)
+				So(sSaved.LastPingKey, ShouldEqual, stats.LastPingKey)
+				So(sSaved.Received, ShouldEqual, stats.Received)
+				So(sSaved.Lost, ShouldEqual, stats.Lost)
+
+				sSaved.Received = 500
+				err = dal.SaveIPStats(sSaved)
+				So(err, ShouldBeNil)
+				sUpdated, err := dal.GetIPStats(sSaved.IP)
+				So(err, ShouldBeNil)
+				So(sUpdated.Received, ShouldEqual, sSaved.Received)
+			})
+			Convey("should return error with invalid IP", func() {
+				_, err := dal.GetIPStats("")
+				So(err, ShouldNotBeNil)
+				So(err.Error(), ShouldContainSubstring, IPRequiredError)
+			})
+			Convey("should return error when it can't open db ", func() {
+				dal.fileName = ""
+				_, err := dal.GetIPStats(stats.IP)
+				So(err, ShouldNotBeNil)
+			})
+			Convey("should return error when it can't deserialize IPStats", func() {
+				dal.Put(stats.IP, []byte("bogus data"), dal.ipStatsBucket)
+				_, err := dal.GetIPStats(stats.IP)
+				So(err, ShouldNotBeNil)
+				So(err.Error(), ShouldContainSubstring, IPStatsDerserializationError)
+			})
+		})
 	})
 }
